85cb3ea0c0262058f18a3a6bc9b0ba1185700146
NestJul <Nest-Ju@ya.ru>
2024-11-14 23:03:01 +0700

    1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 20000; // стоимость билета
+        int rub = 20; // рублей
+        int mile = 1; //миль начислено
+
+        System.out.println("Купили билет,начислено миль: ");
+        System.out.println(( ticket / rub)+ " миль начислено");
+
+
+    }
+}
