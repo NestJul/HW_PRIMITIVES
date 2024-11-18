73b619ca506dfbb6ee4857e93fccf9cc17950c7d
NestJul <Nest-Ju@ya.ru>
2024-11-18 22:13:31 +0700

    2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticket = 20000; // стоимость билета
-        int rub = 20; // рублей
-        int mile = 1; //миль начислено
-
-        System.out.println("Купили билет,начислено миль: ");
-        System.out.println(( ticket / rub)+ " миль начислено");
-
-
+        int ticketRub = 20000; // стоимость билета
+        int rubToMile = 20; // рублей за милю
+        System.out.println("Купили билет: ");
+        System.out.println((ticketRub / rubToMile) + " миль начислено");
     }
 }
